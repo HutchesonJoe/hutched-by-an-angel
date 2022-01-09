fa7ab436e68b202497d5f3a860209529b3c9e748
Joe <hutcheson2001@yahoo.com>
2022-01-09 09:53:02 -0500

    add script.js, connect to HTML & CSS

---
+++ script.js
@@ -0,0 +1,9 @@
+const h3 = document.createElement("h3");
+h3.textContent = "Fitness should feel good.";
+
+document.querySelector("body").appendChild(h3);
+
+const bio = document.createElement("div");
+bio.textContent = "Joe Hutcheson is a personal trainer, yoga instructor, college teacher, and aspiring coder.";
+
+document.querySelector("h3").appendChild(bio);
